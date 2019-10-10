d8b4ef348056ee1dac6695c4267aeb3b4279765a
alik <diaceu@gmail.com>
2019-10-10 14:14:50 +0200

    Task

---
+++ src/main/java/com/example/demo/model/Task.java
@@ -0,0 +1,54 @@
+package com.example.demo.model;
+
+import javax.persistence.*;
+
+@Entity()
+@Table(name = "task")
+@SequenceGenerator(name = "task_id_seq", sequenceName = "task_id_seq", initialValue = 1, allocationSize = 1)
+public class Task {
+    @Id
+    @Column(name = "task_id")
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "task_id_seq")
+    private Long id;
+
+    @Column(name = "task_name", nullable = false)
+    private String name;
+
+    @ManyToOne(cascade = CascadeType.DETACH, fetch = FetchType.EAGER)
+    @Column(name = "task_user_id")
+    private User cratedBy;
+
+    public Task() {
+    }
+
+    public Task(String name, User cratedBy) {
+        this.name = name;
+        this.cratedBy = cratedBy;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public User getCratedBy() {
+        return cratedBy;
+    }
+
+    public void setCratedBy(User cratedBy) {
+        this.cratedBy = cratedBy;
+    }
+}
---
+++ src/main/java/com/example/demo/repo/TaskRepository.java
@@ -0,0 +1,15 @@
+package com.example.demo.repo;
+
+import com.example.demo.model.Task;
+import com.example.demo.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface TaskRepository extends JpaRepository<Task, Long> {
+    List<Task> findAllByUser(User user);
+}
